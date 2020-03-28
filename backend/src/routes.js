a391cf62a28b33f61d4183642d6f4ad01b380ecb
Raphael Lopes <decmaster10@hotmail.com>
2020-03-27 22:41:28 -0300

    Melhorias do codigo, e apilicação de TDD

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,21 +1,55 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const ongController = require('./controllers/OngController')
 const incidentController = require('./controllers/IncidentController')
 const profileController = require('./controllers/ProfileController')
 const sessionController = require('./controllers/SessionController')
 
 const routes = express.Router();
-routes.post('/sessions', sessionController.create);
+routes.post('/sessions',  celebrate({ 
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required(),
+    })
+}), sessionController.create);
 
 routes.get('/ongs', ongController.index);
-routes.post('/ongs', ongController.create);
+routes.post('/ongs', celebrate({ 
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city:  Joi.string().required(), 
+        uf: Joi.string().required().length(2)
+    })
+ }) ,ongController.create);
 
-routes.get('/incident', incidentController.index);
-routes.post('/incident', incidentController.create);
-routes.delete('/incident/:id', incidentController.delete);
+routes.get('/incident', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    }),
+}),  incidentController.index);
 
-routes.get('/profile', profileController.index);
+routes.post('/incident',  celebrate({
+    [Segments.HEADERS]: Joi.object( {
+        authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required().min(10).max(11),
+    })
+}), incidentController.create);
 
+routes.delete('/incident/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    }),
+}) , incidentController.delete);
 
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object( {
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), profileController.index);
 
 module.exports = routes;
---
+++ backend/src/tests/integration/ong.spec.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const app = require('../../app');
+const connection = require('../../database/connection')
+
+describe('ONG', () => {
+    beforeEach(async () =>{
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    it('should be able to create new ONG', async ()=>{
+        const response = await request(app)
+        .post('/ongs')
+        .send({
+            name: "APAD2",
+            email: "APAD@hotmail.com",
+            whatsapp: "11987926101",
+            city: "Sao Paulo",
+            uf: "SP"
+        })
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+})
